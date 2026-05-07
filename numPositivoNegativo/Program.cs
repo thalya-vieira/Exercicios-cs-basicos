10a275f8030505284fc7ad1c1d91ce9f44589d5d
thalya vieira <thalyahadassavieira@gmail.com>
2026-05-07 08:37:49 -0400

    Program que reconhece se o numero e posistivo, negativo ou zero

---
+++ numPositivoNegativo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um numero: ");
+        double num = Convert.ToDouble(Console.ReadLine());
+
+        if (num == 0)
+        {
+            Console.WriteLine("O numero é zero");
+        }
+        else if (num >0)
+        {
+            Console.WriteLine("O numero é positivo");
+        }
+        else
+        {
+            Console.WriteLine("O numero é negativo");
+        }
+
+    }
+}
