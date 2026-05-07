28d99a06b02966e99a7edfab057dae39dbb73ba4
thalya vieira <thalyahadassavieira@gmail.com>
2026-05-06 21:50:02 -0400

    adicionei o arquivo porcentagem de salario

---
+++ porcentagemSalario/Program.cs
@@ -0,0 +1,16 @@
+﻿﻿using System;
+using System.Threading.Channels;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite seu salario: ");
+        double salario = Convert.ToDouble(Console.ReadLine());
+        double porcentagem = (salario*10)/100;
+
+        Console.WriteLine("Seu salario de R$"+salario+" tera um bonus de R$10"+porcentagem);
+        Console.WriteLine("Novo salário: R$"+(salario+porcentagem));
+
+    }
+}
