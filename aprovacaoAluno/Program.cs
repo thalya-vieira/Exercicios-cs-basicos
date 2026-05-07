c692d586860aa6007f1eaba00b930bdfc67eaa25
thalya vieira <thalyahadassavieira@gmail.com>
2026-05-07 00:18:50 -0400

    Adicionando programa de notas e aprovados

---
+++ aprovacaoAluno/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite sua nota: ");
+        double nota = Convert.ToDouble(Console.ReadLine());
+
+        if(nota < 0){
+            Console.WriteLine("Inválido");
+        }
+        if(nota < 5){
+            Console.WriteLine("Reprovado");
+        }
+        else if(nota < 7){
+            Console.WriteLine("Recuperacao");
+        }
+        else if(nota <= 10){
+            Console.WriteLine("Aprovado");
+        }
+        else{
+            Console.WriteLine("Inválido");
+        }
+    }
+}
