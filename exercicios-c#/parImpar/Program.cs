75d9a9a01f5f6445ca1c6359b3379f0d0f84197f
thalya vieira <thalyahadassavieira@gmail.com>
2026-05-06 20:56:03 -0400

    Add files via upload
    primeiros arquivos enviado

---
+++ exercicios-c#/parImpar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um numero: ");
+        double num = Convert.ToDouble(Console.ReadLine());
+
+        if(num%2 == 0)
+        {
+            Console.WriteLine("O numero é par");
+        }
+        else
+        {
+            Console.WriteLine("O numero é impar");
+        }
+    }
+}
+
---
+++ exercicios-c#/rendaIdade/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Idade: ");
+        double idade = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Renda: ");
+        double renda = Convert.ToDouble(Console.ReadLine());
+
+        if(idade>=18 && renda >= 2000)
+        {
+            Console.WriteLine("Crédito liberado");
+        }
+        else
+        {
+            Console.WriteLine("Crédito negado");
+        }
+    }
+}
